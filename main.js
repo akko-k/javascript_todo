e7c2b4230c412984ec1cd709dcfbb179853af3cb
kawakami <barcarolle.op60.horowitz@gmail.com>
2022-01-01 11:15:22 +0900

    タスクの追加機能（テキスト）を実装

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("test");
+const onClickAdd = () => {
+  //テキストボックスの入力値を保存し，初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li 作成
+  const li = document.createElement("li");
+
+  // div 作成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // p 作成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // divタブの中にpタブを入れる
+  div.appendChild(p);
+
+  // liタブの中にdivタブを入れる
+  li.appendChild(div);
+  console.log(li);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+document
+  .getElementById("add_button")
+  .addEventListener("click", () => onClickAdd());
